12e3b69e4a4ca4894415155a834c7d151a925a15
Norton Lam <norton@theponytailposse.com>
2015-10-19 13:32:11 -0500

    refactor: removed unused methods and imports

--- app/src/main/java/com/nortonlam/popularmovies/model/Movie.java
+++ app/src/main/java/com/nortonlam/popularmovies/model/Movie.java
@@ -57,104 +57,20 @@ public class Movie implements Parcelable {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getOriginalTitle() {
-        return originalTitle;
-    }
-
-    public void setOriginalTitle(String originalTitle) {
-        this.originalTitle = originalTitle;
-    }
-
     public String getOverview() {
         return overview;
-    }
-
-    public void setOverview(String overview) {
-        this.overview = overview;
-    }
-
-    public double getPopularity() {
-        return popularity;
-    }
-
-    public void setPopularity(double popularity) {
-        this.popularity = popularity;
-    }
-
-    public boolean isVideo() {
-        return video;
-    }
-
-    public void setVideo(boolean video) {
-        this.video = video;
-    }
-
-    public boolean isAdult() {
-        return adult;
-    }
-
-    public void setAdult(boolean adult) {
-        this.adult = adult;
-    }
-
-    public String getBackdropPath() {
-        return backdropPath;
-    }
-
-    public void setBackdropPath(String backdropPath) {
-        this.backdropPath = backdropPath;
-    }
-
-    public List<Integer> getGenreIdList() {
-        return genreIdList;
-    }
-
-    public void setGenreIdList(List<Integer> genreIdList) {
-        this.genreIdList = genreIdList;
-    }
-
-    public String getOriginalLanguage() {
-        return originalLanguage;
-    }
-
-    public void setOriginalLanguage(String originalLanguage) {
-        this.originalLanguage = originalLanguage;
     }
 
     public Date getReleaseDate() {
         return releaseDate;
     }
 
-    public void setReleaseDate(Date releaseDate) {
-        this.releaseDate = releaseDate;
-    }
-
     public String getPosterPath() {
         return posterPath;
     }
 
-    public void setPosterPath(String posterPath) {
-        this.posterPath = posterPath;
-    }
-
     public String getVoteAverage() {
         return voteAverage;
-    }
-
-    public void setVoteAverage(String voteAverage) {
-        this.voteAverage = voteAverage;
-    }
-
-    public String getVoteCount() {
-        return voteCount;
-    }
-
-    public void setVoteCount(String voteCount) {
-        this.voteCount = voteCount;
     }
 
     public String toString() {
--- app/src/main/java/com/nortonlam/popularmovies/model/TmdbConfiguration.java
+++ app/src/main/java/com/nortonlam/popularmovies/model/TmdbConfiguration.java
@@ -1,19 +1,8 @@
 package com.nortonlam.popularmovies.model;
 
-import android.content.Context;
-import android.util.Log;
-
 import com.google.gson.annotations.SerializedName;
-import com.nortonlam.popularmovies.R;
-import com.nortonlam.popularmovies.net.TheMovieDb;
-import com.nortonlam.popularmovies.net.TheMovieDbApi;
 
 import java.util.List;
-
-import retrofit.Call;
-import retrofit.Callback;
-import retrofit.Response;
-import retrofit.Retrofit;
 
 /**
  * Created by norton
@@ -27,18 +16,6 @@ public class TmdbConfiguration {
 
     public Images getImages() {
         return images;
-    }
-
-    public void setImages(Images images) {
-        this.images = images;
-    }
-
-    public List<String> getChangeKeys() {
-        return changeKeys;
-    }
-
-    public void setChangeKeys(List<String> changeKeys) {
-        this.changeKeys = changeKeys;
     }
 
     public class Images {
@@ -63,60 +40,8 @@ public class TmdbConfiguration {
             return baseUrl + getPosterSizes().get(PREFERRED_POSTER_SIZE);
         }
 
-        public String getBaseUrl() {
-            return baseUrl;
-        }
-
-        public void setBaseUrl(String baseUrl) {
-            this.baseUrl = baseUrl;
-        }
-
-        public String getSecureBaseUrl() {
-            return secureBaseUrl;
-        }
-
-        public void setSecureBaseUrl(String secureBaseUrl) {
-            this.secureBaseUrl = secureBaseUrl;
-        }
-
-        public List<String> getBackdropSizes() {
-            return backdropSizes;
-        }
-
-        public void setBackdropSizes(List<String> backdropSizes) {
-            this.backdropSizes = backdropSizes;
-        }
-
-        public List<String> getLogoSizes() {
-            return logoSizes;
-        }
-
-        public void setLogoSizes(List<String> logoSizes) {
-            this.logoSizes = logoSizes;
-        }
-
         public List<String> getPosterSizes() {
             return posterSizes;
-        }
-
-        public void setPosterSizes(List<String> posterSizes) {
-            this.posterSizes = posterSizes;
-        }
-
-        public List<String> getProfileSizes() {
-            return profileSizes;
-        }
-
-        public void setProfileSizes(List<String> profileSizes) {
-            this.profileSizes = profileSizes;
-        }
-
-        public List<String> getStillSizes() {
-            return stillSizes;
-        }
-
-        public void setStillSizes(List<String> stillSizes) {
-            this.stillSizes = stillSizes;
         }
     }
 }
